444380ddacf50120fb17a251ce12c08fd5bf16f6
jordyhers <jordy925@gmail.com>
2022-06-21 10:40:13 +0300

    [implementation] theme.dart added, Custon colors and SizeConfig

---
+++ lib/src/ui/pages/movie_page.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:movieapp/src/core/constants/strings.dart';
+import 'package:movieapp/src/core/database/movie_database.dart';
+
+import 'package:movieapp/src/core/utils/size_config.dart';
+import 'package:movieapp/src/ui/widgets/movie_card.dart';
+import 'package:provider/provider.dart';
+
+class MoviePage extends StatefulWidget {
+  final MovieDatabase database;
+  const MoviePage({Key? key,required this.database}) : super(key: key);
+
+  /// The [context]  here is the context of the JobsPage
+  ///
+  /// as the result we can get the provider of Database
+  static create(BuildContext context  )  {
+    final database = Provider.of<MovieDatabase>(context,listen: false);
+    return  ChangeNotifierProvider<MovieDatabase>(
+      create: (_) => MovieDatabase.instance,
+      child: Consumer<MovieDatabase>(
+        builder: (context, model, child) => MoviePage(database: database),
+      ),
+    );
+
+  }
+  @override
+  State<MoviePage> createState() => _MoviePageState();
+}
+
+class _MoviePageState extends State<MoviePage> {
+  MovieDatabase get database => widget.database;
+
+  @override
+  void dispose() {
+    database.close();
+    super.dispose();
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    SizeConfig().init(context);
+    return  CupertinoPageScaffold(
+              child: CustomScrollView(
+                slivers: [
+                  SliverAppBar(
+                    elevation: 0,
+                    backgroundColor: Theme
+                        .of(context)
+                        .appBarTheme
+                        .backgroundColor,
+                    expandedHeight: SizeConfig.screenHeight! * 0.15,
+                    floating: true,
+                    pinned: true,
+                    flexibleSpace: FlexibleSpaceBar(
+                      centerTitle: false,
+                      title: Text(UiText.appTitle, style: TextStyle(color: Theme
+                          .of(context)
+                          .textTheme
+                          .bodyText1
+                          ?.color),),
+                    ),
+                  ),
+                  SliverPadding(
+                    padding:  EdgeInsets.only(bottom: SizeConfig.screenHeight! * 0.15),
+                    sliver: SliverList(
+                      delegate: SliverChildBuilderDelegate(
+                          childCount: movieList.length,
+                              (BuildContext context, int index) {
+                            return MovieCard(movie: movieList[index]);
+                          }
+                      ),
+                    ),
+                  ),
+                ],
+                // This trailing comma makes auto-formatting nicer for build methods.
+              ));
+
+  }
+
+
+}
---
+++ lib/src/ui/theme/theme.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+
+Color mainColor = const Color(0xFF696CFF);
+Color secondColor = const Color(0xFF25245F);
+Color inactiveColor = const Color(0xFF778699);
+Color textColor = const Color(0xFF2C3F58);
+Color subtitleColor = const Color(0xFF78C838F);
+Color titleColor = const Color(0xFF343F53);
+
+
+///
+///This Function creates a material color from HEX color value
+///To create a custom theme and a Primary swatch a custom color is entered
+///in the function.
+MaterialColor buildMaterialColor(Color color) {
+  List strengths = <double>[.05];
+  Map<int, Color> swatch = {};
+  final int r = color.red, g = color.green, b = color.blue;
+
+  for (int i = 1; i < 10; i++) {
+    strengths.add(0.1 * i);
+  }
+  for (var strength in strengths) {
+    final double ds = 0.5 - strength;
+    swatch[(strength * 1000).round()] = Color.fromRGBO(
+      r + ((ds < 0 ? r : (255 - r)) * ds).round(),
+      g + ((ds < 0 ? g : (255 - g)) * ds).round(),
+      b + ((ds < 0 ? b : (255 - b)) * ds).round(),
+      1,
+    );
+  }
+  return MaterialColor(color.value, swatch);
+}
+
+class AppTheme {
+   const AppTheme();
+
+  static  ThemeData appTheme = ThemeData(
+      backgroundColor: Colors.white,
+      appBarTheme: const AppBarTheme(backgroundColor: Colors.white),
+      primarySwatch: buildMaterialColor(mainColor),
+      brightness: Brightness.light,
+      textTheme: TextTheme(bodyText1: GoogleFonts.inter(color: titleColor,fontSize: 18),caption: GoogleFonts.inter(color: subtitleColor,fontSize: 12),
+      bodyText2: GoogleFonts.inter(fontSize: 16,color: titleColor,fontWeight: FontWeight.w700,),),
+      iconTheme:  IconThemeData(color: textColor),
+      bottomAppBarColor: Colors.white,
+      dividerColor: Colors.black.withOpacity(0.7),
+      primaryTextTheme: const TextTheme(bodyText2: TextStyle(color: Colors.black))
+  );
+}
